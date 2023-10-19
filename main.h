a5cb0b1aaa6f3cd81bebfa6cc3136c9a9b934642
KelvenLeopoldo <kelvenkalo28.kk@gmail.com>
2023-10-19 13:11:34 -0700

    main.h update

--- main.h
+++ main.h
@@ -8,5 +8,10 @@ int printf_funct_char(va_list types, char buffer[], int flags, int width, int pr
 int printf_funct_int(va_list types, char buffer[], int flags, int width, int precision, int size);
 int printf_funct_percent(va_list types, char buffer[], int flags, int width, int precision, int size);
 int printf_funct_string(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexa(va_list types, char map_to[], char buffer[], int flags, char flag_ch, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[], int flags, int width, int precision, int size);
+int printf_funct_o(va_list types, char buffer[], int flags, int width, int precision, int size);
+int printf_funct_u(va_list types, char buffer[], int flags, int width, int precision, int size);
 
 #endif
